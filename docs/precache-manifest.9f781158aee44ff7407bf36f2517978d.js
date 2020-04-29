74a8df560c93a93e8f45874f885776980fa049d1
sixertoy <sixertoy.github@gmail.com>
2020-04-29 15:56:41 +0200

    create docs folder

---
+++ docs/precache-manifest.9f781158aee44ff7407bf36f2517978d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ef656929c8994bfe80434f89864fac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a5126047230fbcb0adb",
+    "url": "/static/css/2.101ed33f.chunk.css"
+  },
+  {
+    "revision": "b676a140fe324ca593de",
+    "url": "/static/css/main.0c155bd4.chunk.css"
+  },
+  {
+    "revision": "5a5126047230fbcb0adb",
+    "url": "/static/js/2.91c25d58.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/static/js/2.91c25d58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b676a140fe324ca593de",
+    "url": "/static/js/main.4b7fc638.chunk.js"
+  },
+  {
+    "revision": "d81c01bffae563748500",
+    "url": "/static/js/runtime-main.4c4fe26b.js"
+  }
+]);
